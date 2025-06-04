091f227701a4034e607a4ccec8457c34be520137
DWE4 <DWE4@DWE4>
2025-06-04 10:21:05 -0600

    Archivos iniciales del proyecto

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ window.onload = function() {
 console.log("Conectado con éxito");
 console.log("Conectado con éxito");
 console.log("Conectado con éxito <--LVMF-->");
+
+console.log("Conectado con éxito --Eunice Chávez--");
